5fe198824fd5e31bb2559f7edeaf093ee132d0fd
Melike KILIC <melike180400@gmail.com>
2019-12-08 04:04:04 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -51,8 +51,8 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  matrix[1][1]= "Ashley"
-  matrix[1][1]
+  matrix[0][2]="Tiny Ewell"
+  matrix[0]
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
 end
